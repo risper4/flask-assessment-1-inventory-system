64afef7aa4b15da491b5a543c4d7d6dd5a4204e6
risper4 <risperwanjikugichia@gmail.com>
2026-07-02 21:45:13 +0300

    GET : retrieve all products

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from data import Product, products, get_product_details
 
 app = Flask(__name__)
 
 
+@app.route('/inventory', methods=['GET'])
+def show_products() :
+    return jsonify(p.to_dict() for p in products)
 
 
 if __name__ == '__main__' :
