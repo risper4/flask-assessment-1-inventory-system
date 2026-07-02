abc30e3fd6195017aa8ed014bea6d9d52cb39e64
risper4 <risperwanjikugichia@gmail.com>
2026-07-02 21:55:35 +0300

    GET : Retrieval of a specific product

--- app.py
+++ app.py
@@ -4,10 +4,17 @@ from data import Product, products, get_product_details
 app = Flask(__name__)
 
 
+# GET : Retrieves all products
 @app.route('/inventory', methods=['GET'])
 def show_products() :
     return jsonify(p.to_dict() for p in products)
 
+
+# GET : Retrieves a specific product
+@app.route('/inventory/<int:id>', methods=['GET'])
+def show_product(id) :
+    event = next((p for p in products if p.id == id), None)
+    return jsonify(event.to_dict())
 
 if __name__ == '__main__' :
     app.run(port=5555, debug=True)
