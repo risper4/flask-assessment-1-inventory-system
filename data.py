8f291706bdc051ca545c0688ec9416ea3985782c
Risper <risperwanjikugichia@gmail.com>
2026-07-02 19:09:38 +0300

    Getting product details from external api

--- data.py
+++ data.py
@@ -1,3 +1,8 @@
+import requests
+from flask import make_response, jsonify
+
+
+# Product class
 class Product() :
     def __init__(self, id, name):
         self.id = id
@@ -6,7 +11,15 @@ class Product() :
     def to_dict(self) :
         return {'id' : self.id, 'name' : self.name}
     
-
+# A list to store all the project instances
 products = []
+
+
+# Finds more product's details using a barcode from the api
+def get_product_details(barcode) :
+    url = f"https://world.openfoodfacts.net/api/v3.6/product/{barcode}.json"
+    response = requests.get(url)
+    data = response.json()
+    return make_response(jsonify(data), 200)
 
 
