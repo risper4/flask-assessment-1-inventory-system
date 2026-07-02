3ced97189d0daaf07a71fed8f8e4341e13ebb567
risper4 <risperwanjikugichia@gmail.com>
2026-07-02 22:06:56 +0300

    POST : Creation of a new product

--- app.py
+++ app.py
@@ -14,7 +14,20 @@ def show_products() :
 @app.route('/inventory/<int:id>', methods=['GET'])
 def show_product(id) :
     event = next((p for p in products if p.id == id), None)
-    return jsonify(event.to_dict())
+    if event :
+        return jsonify(event.to_dict()), 200
+    else : 
+        return jsonify("Product not found"), 404
+
+
+# POST : Creates new product
+@app.route('/inventory', methods=['POST'])
+def create_product() :
+    data = request.get_json()
+    new_id = max((p.id for p in products)) + 1 if products else 1
+    new_product = Product(id=new_id, name=data['name'])
+    products.append(new_product)
+    return jsonify(new_product.to_dict()), 200
 
 if __name__ == '__main__' :
     app.run(port=5555, debug=True)
